88dbf95abd4a6ed9efb5eb9f6b15dd0744d0b610
Jinhyuk Lee <eces92@gmail.com>
2018-12-04 16:52:43 +0900

    Basic eventemitter

--- src/lemon.js
+++ src/lemon.js
@@ -1,6 +1,9 @@
 /**
  * EventEmitter
  */
+require('debugs/init')
+const debug = require('debug')('lemon:test')
+
 const EventEmitter = require('events')
 class LemonEventEmitter extends EventEmitter {
   constructor() {
--- test/example.js
+++ test/example.js
@@ -10,3 +10,12 @@ test('init', async t => {
   t.is(api.lemonized, true)
   t.pass()
 })
+test('throw away', async t => {
+  const api = new LemonEventEmitter()
+  t.plan(2)
+  api.on('hey', r => {
+    t.is(r, 'data')
+  })
+  const hadListener = api.emit('hey', 'data')
+  t.is(hadListener, true)
+})
