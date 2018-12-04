0dedba69e6c80d84a0e92cea86fb19f5f40afdb2
Jinhyuk Lee <eces92@gmail.com>
2018-12-04 16:56:03 +0900

    Add use, emit, on

--- src/lemon.js
+++ src/lemon.js
@@ -10,5 +10,18 @@ class LemonEventEmitter extends EventEmitter {
     super()
     this.lemonized = true
   }
+
+  use(middleware) {
+
+  }
+  use(scope, middleware) {
+
+  }
+  emit(eventName, ...args) {
+    return super.emit(eventName, ...args)
+  }
+  on(eventName, listener) {
+    return super.on(eventName, listener)
+  }
 }
 module.exports = LemonEventEmitter
